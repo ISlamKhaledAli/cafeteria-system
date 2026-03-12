e0358e23e4951d5d2568023c58002f149fdcf162
Islam Khaled <khaledislam9003@gmail.com>
2026-03-12 23:28:37 +0200

    add login view with form and layout includes

---
+++ views/auth/login.php
@@ -0,0 +1,19 @@
+<?php include "../../layouts/header.php"; ?>
+
+<div class="container mt-5">
+
+<h3>Login</h3>
+
+<form>
+
+<input type="email" class="form-control mb-3" placeholder="Email">
+
+<input type="password" class="form-control mb-3" placeholder="Password">
+
+<button class="btn btn-primary">Login</button>
+
+</form>
+
+</div>
+
+<?php include "../../layouts/footer.php"; ?>
