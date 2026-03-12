79119ee8075a92a40713e3fd965fd6281131647a
Islam Khaled <khaledislam9003@gmail.com>
2026-03-12 23:19:03 +0200

    add initial project files including controllers, models, views, and layout components

---
+++ layouts/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Cafeteria System</title>
+
+<link rel="stylesheet" href="/assets/css/style.css">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
---
+++ layouts/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-dark bg-dark">
+
+<div class="container">
+
+<a class="navbar-brand">Cafeteria</a>
+
+<ul class="nav">
+
+<li class="nav-item"><a class="nav-link text-white" href="#">Home</a></li>
+
+<li class="nav-item"><a class="nav-link text-white" href="#">My Orders</a></li>
+
+<li class="nav-item"><a class="nav-link text-white" href="#">Products</a></li>
+
+</ul>
+
+</div>
+
+</nav>
